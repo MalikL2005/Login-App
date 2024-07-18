f256e632049c643d2ea184a7842ad63b79ee3c2b
MalikL2005 <malik.lorenz2005@gmail.com>
2024-07-18 19:45:17 +0200

    second commit

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
-import express from "express"
-import mongoose from "mongoose"
-import dotenv from "dotenv"
-import path from 'path'
-import fileURLToPath from 'url'
+const express = require("express")
+const mongoose = require("mongoose")
+const dotenv = require("dotenv")
+const path = require('path')
+const {fileURLToPath} = require('url')
+const bodyParser = require('body-parser');
+
 
 //DB Link: https://cloud.mongodb.com/v2/66821979b1c025127e852e14#/metrics/replicaSet/66917f0aa266f14f1f54d0dd/explorer/sample_mflix/users/find
 //Host Link: http://localhost:8000/
@@ -11,9 +13,8 @@ dotenv.config()
 const MONGO_URI = process.env.MONGO_URI
 const app = express()
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 //open server & db connection 
 const PORT = process.env.PORT || 7000
@@ -24,36 +25,71 @@ mongoose.connect(MONGO_URI).then(()=>{
     })
 }).catch((error) => {console.error(error)})
 const db = mongoose.connection
+const collection = db.collection('users')
 
 
 
-app.get('/users', async(req, res)=>{
-    const collection = db.collection('users')
-    try{
-        let data = await collection.find({}).toArray()
-        res.send(data)
-    }
-    catch(err){
-        res.status(500).json({message: err})
-    }
-})
-
-app.get('/hello', (req, res)=> {
-    res.send("Hello")
-})
-
-app.get('/getUsers', async (req, res)=>{
-    const collection = db.collection('users')
-    let searched_name = req.query.name
-    let data = await collection.find({name:searched_name}).toArray()
-    if (data.length){
-        res.json(data)
-    }
-    else{res.send(`Could not find user with the name ${searched_name}.`)}
-
-})
 
 
 app.get('/', (req, res)=> {
     res.sendFile(__dirname + '/views/index.html');
 })
+
+app.get('/users', async(req, res)=>{
+    try{
+        let data = await collection.find({}).toArray()
+        res.send(data)
+    }
+    catch(err){ res.status(500).json({message: err})}
+})
+
+
+app.get('/getUsers', async (req, res)=>{
+    let searched_name = req.query.name
+    let data = await collection.find({name:searched_name}).toArray()
+    if (data.length){ res.json(data)}
+    else{ res.send(`Could not find user with the name ${searched_name}.`)}
+})
+
+app.post('/add_user', async (req, res)=>{
+    let user_mail_exists = await collection.findOne({email: req.body.new_email})
+    if (user_mail_exists){
+        res.send(`<h1>Email already exists</h1><a href="/sign_in">Go to Sign in form</a>`)
+        return
+    }
+    if(req.body.new_name && req.body.new_email && req.body.new_pw){
+        var add_user = collection.insertOne({"name": req.body.new_name, "email": req.body.new_email, "password": req.body.new_pw})
+        res.send("You have successfully created an account.")
+    } else{ res.send("Please fill out every field.")}
+})
+
+
+app.post('/remove_user', (req, res)=>{
+    console.log(req.body.remove_user_name)
+    try{
+        collection.deleteOne({'name': req.body.remove_user_name})
+        res.send(`User ${req.body.remove_user_name} has been removed`)
+    } catch(e){
+        res.send("Something went wrong " + e)
+    }
+})
+
+app.get('/sign_in', (req, res)=>{
+    res.sendFile(__dirname + '/views/sign_in.html')
+})
+
+app.get('/signing_in', async (req, res)=>{
+    user = await collection.findOne({name: req.query.user_name, email: req.query.user_email, password: req.query.user_pw}, (err, result)=>{
+        if(!err){
+            if(result){
+                res.send("You have signed in.")
+            } else{
+                res.send("Password didn't match username/email.")
+            }
+        }
+        else{
+            res.send("MongoDB error")
+        }
+    })
+})
+
